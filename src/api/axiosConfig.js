a5f70948c37169b668eac23831caa9b3b80b8707
Newlifejay <ugochukwuanani@gmail.com>
2026-04-09 01:07:10 +0100

    I modified the axiosConfig.js file

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://{your-backend-url}/api',
+    baseURL:'http://172.211.141.98:8080',
     headers: {
         'Content-Type': 'application/json',
     },
